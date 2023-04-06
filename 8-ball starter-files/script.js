bd10b52af85ff1c33570c087c8b96a961959c49c
cloudarcKnights <126310351+cloudarcKnights@users.noreply.github.com>
2023-04-06 09:41:34 -0700

    Developed 8-ball API

---
+++ 8-ball starter-files/script.js
@@ -0,0 +1,87 @@
+// API
+const ballSelector = document.querySelector('#ball');
+const buttonSelector = document.querySelector('#button');
+const inputSelector = document.querySelector('#input');
+const answerSelector = document.querySelector('#answer')
+const errorSelector = document.querySelector('#error')
+const API_ENDPOINT = 'https://yesno.wtf/api';
+
+let isRequestInProgress = false;
+
+/**
+ * STEPS:
+ *
+ * 1. Create a fetchAnswer function and call the API
+ * 2. Output the API's response
+ * 3. Attach fetchAnswer to an event listener
+ * 4. Clear output after 3 seconds
+ * 5. Optional: add loading/error states
+ *
+ */
+
+const setIsRequestInProgress = value => {
+    isRequestInProgress = value;
+}
+const setDisableButtonState = isDisabling => {
+    if (isDisabling) {
+        buttonSelector.setAttribute('disabled', 'disabled');
+    } else {
+        buttonSelector.removeAttribute('disabled');
+    }    
+} ;
+
+
+const cleanupResponse = () => {
+    setTimeout(() => {
+        answerSelector.innerHTML = '';
+        inputSelector.value = '';
+        setIsRequestInProgress(false);
+        setDisableButtonState(false);
+    }, 3000);    
+};
+
+const showAnswer = answer => {
+    setTimeout(() => {
+        document.querySelector('#answer').innerHTML = `<p>${answer}</p>`;
+        ballSelector.classList.remove('shake__ball');
+        cleanupResponse();
+    }, 1000);
+};
+
+
+const fetchAnswer = () => {
+    setIsRequestInProgress(true);
+
+    setDisableButtonState(true);
+    ballSelector.classList.add('shake__ball');
+
+    fetch(API_ENDPOINT)
+        .then(data => data.json())
+        .then(data => showAnswer(data.answer));
+};
+
+const showError = () => {
+    errorSelector.innerHTML = 'You need to type your question';
+
+    setTimeout(() => {
+        errorSelector.innerHTML = '';
+    }, 3000);
+};
+
+
+const getAnswer = () => {
+    if (isRequestInProgress) return;
+    if (!inputSelector.value) return showError();
+    fetchAnswer();
+
+}
+
+const handleKeyEnter = e => {
+    if (e.keyCode === 13) {
+        getAnswer();
+    }
+};
+
+
+
+buttonSelector.addEventListener('click', getAnswer);
